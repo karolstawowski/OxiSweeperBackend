c37e7b7ad59718dd9e2e5042d53918452eb6fc9f
Karol Stawowski <66377130+karolstawowski@users.noreply.github.com>
2023-01-25 16:36:42 +0100

    feat: extend logging informations

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -9,11 +9,25 @@ use Illuminate\Support\Facades\Hash;
 use Laravel\Sanctum\PersonalAccessToken;
 use Illuminate\Support\Facades\Storage;
 
+
+
+function store_login_logs($log_file_name, $user_info, $username, $is_logged_in_correctly) {
+    if (Storage::disk('local')->exists($log_file_name)) {
+        Storage::disk('local')->append($log_file_name, $username . ", " . date("Y.m.d H:i:s") . ", " . $is_logged_in_correctly . ", " . $user_info);
+    } else {
+        Storage::disk('local')->put($log_file_name, "user_name, date, is_logged_correctly, user_info \r\n" . $username . ", " . date("Y.m.d H:i:s") . ", " . $is_logged_in_correctly . ", " . $user_info);
+    }
+}
+
 class AuthController extends Controller
 {
     public function register(Request $request)
     {
+        $log_file_name = 'loging_in_logs.csv';
+
         $fields = $request->validate(['name' => 'required|string', 'password' => 'required|string']);
+
+        store_login_logs($log_file_name, $request->server('HTTP_USER_AGENT'), $fields['name'], true);
 
         $user = Users::create(['name' => $fields['name'], 'password' => bcrypt($fields['password'])]);
 
@@ -51,13 +65,9 @@ class AuthController extends Controller
             'password' => 'required|string',
         ]);
 
-        if (Storage::disk('local')->exists($log_file_name)) {
-            Storage::disk('local')->append($log_file_name, $fields['name'] . "," . date("Y.m.d H:i:s"));
-        } else {
-            Storage::disk('local')->put($log_file_name, "user_name, date \r\n" . $fields['name'] . ", " . date("Y.m.d H:i:s"));
-        }
-
         $user = Users::where('name', $fields['name'])->first();
+
+        store_login_logs($log_file_name, $request->server('HTTP_USER_AGENT'), $fields['name'], $user ? "1" : "0");
 
         if (!$user || !Hash::check($fields['password'], $user->password)) {
             return response([
